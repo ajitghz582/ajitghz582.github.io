a887ebd3aa43c25e1daf65a495d1aad4e3cd04a0
ajitghz582 <ajitghz582@gmail.com>
2015-09-12 13:33:46 +0530

    Signed-off-by: ajitghz582 <ajitghz582@gmail.com>

---
+++ first_page.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js" >
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!--[if IE]>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<![endif]-->
+
+<title>Online Tour & Travels</title>
+
+<!-- BOOTSTRAP CORE CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<!-- ION ICONS STYLES -->
+<link href="assets/css/ionicons.css" rel="stylesheet" />
+<!-- FONT AWESOME ICONS STYLES -->
+<link href="assets/css/font-awesome.css" rel="stylesheet" />
+<!-- FANCYBOX POPUP STYLES -->
+<link href="assets/js/source/jquery.fancybox.css" rel="stylesheet" />
+<!-- STYLES FOR VIEWPORT ANIMATION -->
+<link href="assets/css/animations.min.css" rel="stylesheet" />
+<!-- CUSTOM CSS -->
+<link href="assets/css/style-blue.css" rel="stylesheet" />
+
+</head>
+<body data-spy="scroll" data-target="#menu-section">
+<!--MENU SECTION START-->
+<div class="navbar navbar-inverse navbar-fixed-top scroll-me" id="menu-section" >
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="#">
+
+ASR Tour & Travels
+
+</a>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav navbar-right">
+<li><a href="#home">HOME</a></li>
+<li><a href="#services">SERVICES</a></li>
+<li><a href="#work">WORK</a></li>
+<li><a href="#team">TEAM</a></li>
+<li><a href="#contact">CONTACT</a></li>
+</ul>
+</div>
+
+</div>
+</div>
+<!--MENU SECTION END-->
+<!--HOME SECTION START-->
+<div id="home" >
+<div class="container">
+<div class="row">
+<div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
+<div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
+
+<div class="carousel-inner">
+<div class="item active">
+
+<h3>
+The World is a Book, Those who donot travel Read Only a Single Page.
+</h3>
+<p>
+Dalai Lama
+</p>
+</div>
+<div class="item">
+<h3>
+Two roads diverged in a wood and I – I took the one less traveled by.
+</h3>
+<p>
+Robert Frost
+</p>
+</div>
+
+</div>
+
+
+</div>
+
+
+</div>
+</div>
+<div class="row animate-in" data-anim-type="fade-in-up">
+<div class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2 scroll-me">
+
+
+<!-- <p >
+This line is fixed so you can write anything
+
+</p> -->
+<!-- <div class="social">
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-facebook "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-twitter"></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-google-plus "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-linkedin "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-pinterest "></i></a>
+<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-github "></i></a>
+</div> -->
+<a href="http://localhost/test/users/signin.php" class=" btn button-custom btn-custom-two">Log In </a>
+<a href="http://localhost/test/users/register.php" class=" btn btn-success">Sign Up </a>
+
+</div>
+</div>
+</div>
+
+</div>
+<!--HOME SECTION END-->
+<!--SERVICE SECTION START-->
+<section id="services" >
+<div class="container">
+<div class="row text-center header">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
+<h3>Our Services</h3>
+<hr />
+</div>
+</div>
+<div class="row animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-android-train"></i>
+<h3>Railway Reswrvation</h3>
+
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-person-stalker"></i>
+<h3>Hotel Reservation</h3>
+
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-android-bus"></i>
+<h3>Bus Booking</h3>
+
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-calendar"></i>
+<h3>Hiliday Packages</h3>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-android-car"></i>
+<h3>Cab Booking</h3>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="services-wrapper">
+<i class="ion-android-plane"></i>
+<h3>Flight Booking</h3>
+</div>
+</div>
+
+
+
+</div>
+</div>
+</section>
+<!--SERVICE SECTION END-->
+
+<!--WORK SECTION START-->
+<section id="work" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<h3>Our Works</h3>
+<hr />
+</div>
+</div>
+<div class="row text-center animate-in" data-anim-type="fade-in-up" >
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pad-bottom">
+<div class="caegories">
+<a href="#" data-filter="*" class="active btn btn-custom btn-custom-two btn-sm">All</a>
+<a href="#" data-filter=".html" class="btn btn-custom btn-custom-two btn-sm">Resort</a>
+<a href="#" data-filter=".css" class="btn btn-custom btn-custom-two btn-sm">Luxory</a>
+<a href="#" data-filter=".code" class="btn btn-custom btn-custom-two btn-sm" >Honeymoon</a>
+<a href="#" data-filter=".script" class="btn btn-custom btn-custom-two btn-sm" >Beach</a>
+</div>
+</div>
+</div>
+<div class="row text-center animate-in" data-anim-type="fade-in-up" id="work-div">
+
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 html">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/11.jpg">
+
+<img src="assets/img/portfolio/11.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>Manali Resort</h4>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 html css">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/12.jpg">
+
+<img src="assets/img/portfolio/12.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>Shimla Resort</h4>
+</div>
+</div>
+<!-- <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 code script"> -->
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 code html script">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/hon1.jpg">
+
+<img src="assets/img/portfolio/hon1.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>Mumbai Resort</h4>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 html script">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/b1.jpg">
+
+<img src="assets/img/portfolio/b1.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>raj re</h4>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 html code">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/hon3.jpg">
+
+<img src="assets/img/portfolio/hon3.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>lux 2</h4>
+</div>
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 script">
+<div class="work-wrapper">
+
+<a class="fancybox-media" title="Image Title Goes Here" href="assets/img/portfolio/6.jpg">
+
+<img src="assets/img/portfolio/6.jpg" class="img-responsive img-rounded" alt="" />
+</a>
+
+<h4>lux 3</h4>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!--WORK SECTION END-->
+<!--TEAM SECTION START-->
+<section id="team" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+<h3>Team Members </h3>
+<hr />
+</div>
+</div>
+<div class="row animate-in" data-anim-type="fade-in-up">
+
+<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+<div class="team-wrapper">
+<div class="team-inner" style="background-image: url('assets/img/team/11.jpg')" >
+<a href="https://www.facebook.com/ajitghz" target="_blank" > <i class="fa fa-facebook" ></i></a>
+</div>
+<div class="description">
+<h3> Ajit Kumar Yadav</h3>
+<h5> <strong> Developer & Designer </strong></h5>
+<p>
+	Its not about the artist, its about the art.
+</p>
+</div>
+</div>
+</div>
+<!--<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+<div class="team-wrapper">
+<div class="team-inner" style="background-image: url('assets/imgteam/2.jpg')" >
+<a href="https://www.facebook.com/prashant.varshney.1029?fref=browse_search" target="_blank" > <i class="fa fa-facebook" ></i></a>
+</div>
+<div class="description">
+<h3> Prashant varshney</h3>
+<h5> <strong> Developer & Designer </strong></h5>
+<p>
+We all have our passion, this is mine.
+</p>
+</div>
+</div>
+</div>-->
+<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+<div class="team-wrapper">
+<div class="team-inner" style="background-image: url('assets/img/team/22.jpg')" >
+<a href="https://www.facebook.com/profile.php?id=100003191935456&fref=ts" target="_blank" > <i class="fa fa-facebook" ></i></a>
+</div>
+<div class="description">
+<h3> Rahul Raj</h3>
+<h5> <strong> Developer & Designer </strong></h5>
+<p>
+We all have our passion, this is mine.
+</p>
+</div>
+</div>
+</div>
+
+<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+<div class="team-wrapper">
+<div class="team-inner" style="background-image: url('assets/img/team/33.jpg')" >
+<a href="https://www.facebook.com/Shivam.mgr?fref=ts" target="_blank" > <i class="fa fa-facebook" ></i></a>
+</div>
+<div class="description">
+<h3> Shivam Kumar</h3>
+<h5> <strong> Developer & Designer </strong></h5>
+<p>
+We all have our passion, this is mine.
+</p>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!--TEAM SECTION END-->
+<!--GRID SECTION START-->
+<section id="grid" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+
+<!--CONTACT SECTION START-->
+<section id="contact" >
+<div class="container">
+<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+<h3>Contact Details </h3>
+<hr />
+
+</div>
+</div>
+
+<div class="row animate-in" data-anim-type="fade-in-up">
+
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>We Are Social</h3>
+<p>
+To Know more about us & For any query feel free to contact us.
+</p>
+<div class="social-below">
+<a href="#" class="btn button-custom btn-custom-two" > Facebook</a>
+<a href="#" class="btn button-custom btn-custom-two" > Twitter</a>
+<a href="#" class="btn button-custom btn-custom-two" > Google +</a>
+</div>
+</div>
+
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Quick Contact</h3>
+<h4><strong>Email : </strong> ajit.iiita@gmail.com </h4>
+<h4><strong>Call : </strong> +07309085586 </h4>
+<h4><strong>Skype : </strong> ajitghz </h4>
+</div>
+
+</div>
+<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+<div class="contact-wrapper">
+<h3>Address : </h3>
+<h4>iiit allahabad </h4>
+<h4>India</h4>
+<div class="footer-div" >
+&copy; 2015 Ajit | <a href="#" target="_blank" >by Ajit</a>
+</div>
+</div>
+
+</div>
+
+</div>
+
+
+</div>
+</section>
+<!--CONTACT SECTION END-->
+
+<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME -->
+<!-- CORE JQUERY -->
+<script src="assets/js/jquery-1.11.1.js"></script>
+<!-- BOOTSTRAP SCRIPTS -->
+<script src="assets/js/bootstrap.js"></script>
+<!-- EASING SCROLL SCRIPTS PLUGIN -->
+<script src="assets/js/vegas/jquery.vegas.min.js"></script>
+<!-- VEGAS SLIDESHOW SCRIPTS -->
+<script src="assets/js/jquery.easing.min.js"></script>
+<!-- FANCYBOX PLUGIN -->
+<script src="assets/js/source/jquery.fancybox.js"></script>
+<!-- ISOTOPE SCRIPTS -->
+<script src="assets/js/jquery.isotope.js"></script>
+<!-- VIEWPORT ANIMATION SCRIPTS   -->
+<script src="assets/js/appear.min.js"></script>
+<script src="assets/js/animations.min.js"></script>
+<!-- CUSTOM SCRIPTS -->
+<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
